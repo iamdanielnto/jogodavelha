70517d262a3e4081472afa4ad6ed9458a42d18e6
Daniel <danielnto@yahoo.com.br>
2022-04-19 19:28:39 -0300

    tabela pronta

--- Quadrado.js
+++ Quadrado.js
@@ -7,18 +7,21 @@ export default function Quadrado()
 
     const [jogador, setJogador] = useState(0)
 
-   let icone = "";
+   let icone = " ";
+
    if (jogador == 1)
    {
-        icone = <Entypo name="cross" size={32} color="black" />
+        icone = <Entypo name="cross" size={32} color="black"/>
    } else if (jogador == 2)
    {
-       icone = <Entypo name="circle" size={32} color="black" />
+       icone = <Entypo name="circle" size={32} color="black"/>
    }
 
     return(
 
-        <View style={css.box}>{icone}</View>
+        <View style={css.box}>
+            {icone}
+            </View>
     );
 }
 
@@ -29,7 +32,7 @@ const css = StyleSheet.create({
         borderStyle: "solid",
         borderWidth: 1,
         width: 80,
-        heigth: 80,
+        height: 80,
         display: 'flex',
         alignItems: 'center',
         justifyContent: 'center'
