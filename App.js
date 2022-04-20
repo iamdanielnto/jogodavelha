695da434dd27303d52157aba1fb75fd0dd76776a
Daniel <danielnto@yahoo.com.br>
2022-04-19 21:56:04 -0300

    vencedor

--- App.js
+++ App.js
@@ -1,30 +1,102 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {useState} from 'react';
 
 import Quadrado from './Quadrado';
 
-export default function App() {
+
+export default function App()
+{
+
+  const [jogador, setJogador] = useState(1);
+  const [tabuleiro, setTabuleiro] = useState([ 0,0,0,  0,0,0,  0,0,0 ]);
+
+  function alteraTabuleiro(pos)
+  {
+
+    const novo = tabuleiro;
+    novo[pos] = jogador
+
+  }
+
+  const [vencedor, setVencedor] = useState(0);
+
+  
+
+  if (tabuleiro [0] !=0 && tabuleiro [0] == tabuleiro[1] && tabuleiro [1] == tabuleiro [2])
+  {
+    setVencedor = (tabuleiro[0]);
+  }
+  if (tabuleiro [3] !=0 && tabuleiro [3] == tabuleiro[4] && tabuleiro [4] == tabuleiro [5])
+  {
+    setVencedor = (tabuleiro[3]);
+  }
+  if (tabuleiro [6] !=0 && tabuleiro [6] == tabuleiro[7] && tabuleiro [7] == tabuleiro [8])
+  {
+    setVencedor = (tabuleiro[6]);
+  }
+
+
   return (
     <View style={styles.container}>
 
+      {(vencedor != 0)? 
+      <Text>O vencedor é {vencedor}</Text>:
+      <Text> O Proximo Jogador é o #{jogador}</Text>}
+
       <View>
-      <Quadrado/>
-      <Quadrado/>
-      <Quadrado/>
-     </View>
+        <Quadrado 
+          posicao = '0' 
+          tabuleiro ={alteraTabuleiro} 
+          jogador = {jogador} 
+          alteraJogador={setJogador}/>
+        <Quadrado 
+          posicao = '1' 
+          tabuleiro ={alteraTabuleiro} 
+          jogador = {jogador} 
+          alteraJogador={setJogador}/> 
+        <Quadrado 
+          posicao = '2' 
+          tabuleiro ={alteraTabuleiro} 
+          jogador = {jogador} a
+          lteraJogador={setJogador}/>
+      </View>
 
-     <View>
-      <Quadrado/>
-      <Quadrado/>
-      <Quadrado/>
-     </View>
+      <View>
+        <Quadrado 
+          posicao = '3' 
+          tabuleiro ={alteraTabuleiro} 
+          jogador = {jogador} 
+          alteraJogador={setJogador}/>
+        <Quadrado 
+          posicao = '4' 
+          tabuleiro ={alteraTabuleiro} 
+          jogador = {jogador} 
+          alteraJogador={setJogador}/> 
+        <Quadrado 
+          posicao = '5' 
+          tabuleiro ={alteraTabuleiro} 
+          jogador = {jogador} 
+          alteraJogador={setJogador}/>
+      </View>
 
-
-     <View>
-      <Quadrado/>
-      <Quadrado/>
-      <Quadrado/>
-     </View>
+      < View>
+        <Quadrado 
+          posicao = '6' 
+          tabuleiro ={alteraTabuleiro} 
+          jogador = {jogador} 
+          alteraJogador={setJogador}/>
+        <Quadrado 
+          posicao = '7' 
+          tabuleiro ={alteraTabuleiro} 
+          jogador = {jogador} 
+          alteraJogador={setJogador}/> 
+        <Quadrado 
+          posicao = '8' 
+          tabuleiro ={alteraTabuleiro} 
+          jogador = {jogador} 
+          alteraJogador={setJogador}/>
+      </View>
 
 
     </View>
@@ -38,6 +110,6 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     alignContent: 'space-between',
-    flexDirection: 'row-reverse'
+    flexDirection: 'row'
   },
 });
--- Quadrado.js
+++ Quadrado.js
@@ -1,27 +1,45 @@
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, TouchableWithoutFeedback} from 'react-native';
 import {Entypo} from '@expo/vector-icons';
 import {useState} from 'react';
 
-export default function Quadrado()
+export default function Quadrado(props)
 {
 
-    const [jogador, setJogador] = useState(0)
+    const jogador = props.jogador
 
-   let icone = " ";
+    const [fezJogada, setJogada]= useState(0)
 
-   if (jogador == 1)
+   let icone = <Text></Text>;
+
+   if (fezJogada == 1)
    {
         icone = <Entypo name="cross" size={32} color="black"/>
-   } else if (jogador == 2)
+   } else if (fezJogada == 2)
    {
        icone = <Entypo name="circle" size={32} color="black"/>
    }
 
-    return(
+   function jogada ()
+   {
+       if(fezJogada == 0 )
+       {
+         setJogada(jogador)
+         const posicao = props.posicao
+         props.tabuleiro(posicao)
+         let novo = (jogador ==1)? 2 : 1;
+         props.alteraJogador(novo);
+       }
 
-        <View style={css.box}>
-            {icone}
+
+
+   }
+
+    return(
+        <TouchableWithoutFeedback onPress={jogada}>
+            <View style={css.box}>
+              {icone}
             </View>
+        </TouchableWithoutFeedback>
     );
 }
 
