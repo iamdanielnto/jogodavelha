d8e87b09d7d2aadf4aacd8e247473e222ab2981b
Daniel <danielnto@yahoo.com.br>
2022-04-22 18:29:50 -0300

    atualizaçao 1

--- App.js
+++ App.js
@@ -1,18 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import {useState} from 'react';
+import { StyleSheet, Text, View, Alert } from 'react-native';
+import { useState } from 'react';
 
 import Quadrado from './Quadrado';
 
 
-export default function App()
-{
+export default function App() {
 
   const [jogador, setJogador] = useState(1);
-  const [tabuleiro, setTabuleiro] = useState([ 0,0,0,  0,0,0,  0,0,0 ]);
+  const [tabuleiro, setTabuleiro] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0]);
 
-  function alteraTabuleiro(pos)
-  {
+  function alteraTabuleiro(pos) {
 
     const novo = tabuleiro;
     novo[pos] = jogador
@@ -21,81 +19,99 @@ export default function App()
 
   const [vencedor, setVencedor] = useState(0);
 
-  
+  const posicoes = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [6, 4, 2],
 
-  if (tabuleiro [0] !=0 && tabuleiro [0] == tabuleiro[1] && tabuleiro [1] == tabuleiro [2])
-  {
-    setVencedor = (tabuleiro[0]);
-  }
-  if (tabuleiro [3] !=0 && tabuleiro [3] == tabuleiro[4] && tabuleiro [4] == tabuleiro [5])
-  {
-    setVencedor = (tabuleiro[3]);
-  }
-  if (tabuleiro [6] !=0 && tabuleiro [6] == tabuleiro[7] && tabuleiro [7] == tabuleiro [8])
-  {
-    setVencedor = (tabuleiro[6]);
+  ]
+
+
+  const ganhador = function (valor) {
+
+    if (vencedor == 0) {
+      const a = valor[0]
+      const b = valor[1]
+      const c = valor[2]
+
+
+      if (tabuleiro[a] != 0 && tabuleiro[a] == tabuleiro[b] && tabuleiro[b] == tabuleiro[c]) {
+        setVencedor(tabuleiro[a])
+      }
+    }
   }
 
+  posicoes.forEach(ganhador);
+
+
+  if (vencedor != 0) {
+    Alert.alert("O vencedor é o jogador" + vencedor)
+  }
 
   return (
     <View style={styles.container}>
 
-      {(vencedor != 0)? 
-      <Text>O vencedor é {vencedor}</Text>:
-      <Text> O Proximo Jogador é o #{jogador}</Text>}
+      {(vencedor != 0) ?
+        <Text/> :
+        <Text> O Proximo Jogador é o #{jogador}</Text>}
 
       <View>
-        <Quadrado 
-          posicao = '0' 
-          tabuleiro ={alteraTabuleiro} 
-          jogador = {jogador} 
-          alteraJogador={setJogador}/>
-        <Quadrado 
-          posicao = '1' 
-          tabuleiro ={alteraTabuleiro} 
-          jogador = {jogador} 
-          alteraJogador={setJogador}/> 
-        <Quadrado 
-          posicao = '2' 
-          tabuleiro ={alteraTabuleiro} 
-          jogador = {jogador} a
-          lteraJogador={setJogador}/>
+        <Quadrado
+          posicao='0'
+          tabuleiro={alteraTabuleiro}
+          jogador={jogador}
+          alteraJogador={setJogador} />
+        <Quadrado
+          posicao='1'
+          tabuleiro={alteraTabuleiro}
+          jogador={jogador}
+          alteraJogador={setJogador} />
+        <Quadrado
+          posicao='2'
+          tabuleiro={alteraTabuleiro}
+          jogador={jogador} a
+          lteraJogador={setJogador} />
       </View>
 
       <View>
-        <Quadrado 
-          posicao = '3' 
-          tabuleiro ={alteraTabuleiro} 
-          jogador = {jogador} 
-          alteraJogador={setJogador}/>
-        <Quadrado 
-          posicao = '4' 
-          tabuleiro ={alteraTabuleiro} 
-          jogador = {jogador} 
-          alteraJogador={setJogador}/> 
-        <Quadrado 
-          posicao = '5' 
-          tabuleiro ={alteraTabuleiro} 
-          jogador = {jogador} 
-          alteraJogador={setJogador}/>
+        <Quadrado
+          posicao='3'
+          tabuleiro={alteraTabuleiro}
+          jogador={jogador}
+          alteraJogador={setJogador} />
+        <Quadrado
+          posicao='4'
+          tabuleiro={alteraTabuleiro}
+          jogador={jogador}
+          alteraJogador={setJogador} />
+        <Quadrado
+          posicao='5'
+          tabuleiro={alteraTabuleiro}
+          jogador={jogador}
+          alteraJogador={setJogador} />
       </View>
 
       < View>
-        <Quadrado 
-          posicao = '6' 
-          tabuleiro ={alteraTabuleiro} 
-          jogador = {jogador} 
-          alteraJogador={setJogador}/>
-        <Quadrado 
-          posicao = '7' 
-          tabuleiro ={alteraTabuleiro} 
-          jogador = {jogador} 
-          alteraJogador={setJogador}/> 
-        <Quadrado 
-          posicao = '8' 
-          tabuleiro ={alteraTabuleiro} 
-          jogador = {jogador} 
-          alteraJogador={setJogador}/>
+        <Quadrado
+          posicao='6'
+          tabuleiro={alteraTabuleiro}
+          jogador={jogador}
+          alteraJogador={setJogador} />
+        <Quadrado
+          posicao='7'
+          tabuleiro={alteraTabuleiro}
+          jogador={jogador}
+          alteraJogador={setJogador} />
+        <Quadrado
+          posicao='8'
+          tabuleiro={alteraTabuleiro}
+          jogador={jogador}
+          alteraJogador={setJogador} />
       </View>
 
 
